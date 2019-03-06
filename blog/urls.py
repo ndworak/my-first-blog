900a1583a1418c8268a0826e807cbe4e9b87dcca
natalie <natalie.hensley@austin.utexas.edu>
2019-03-06 14:09:02 -0600

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     url('^$', views.post_list, name="post_list"),
     url('^post/(\d+)/$', views.post_detail, name='post_detail'),
+    url('^post/new/$', views.post_new, name='post_new'),
+    url('post/(\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
